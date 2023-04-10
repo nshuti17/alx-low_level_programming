0eaadfb193ae965c6e8cfb1cb51b2397e77f6f41
nshuti17 <nshutimartial17@gmail.com>
2023-04-10 04:20:11 -0700

    commit on the second fileo f th project

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+/**
+ * create_file - append to file
+ * Description: Create a file.
+ * @filename: path to file
+ * @text_content: content
+ * Return: chars read
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, new_doc, k = 0;
+
+	/* Check if filename is NULL and return -1 */
+	if (!filename)
+		return (-1);
+	/*
+	 * Open file for write only, create it if it doesn't exist
+	 * Truncate to zero length if it exists
+	 * 0600, Sets file permissions to rw- --- ---, read & write for owner only
+	 */
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	/* If text_content is not empty, write it to file */
+	if (text_content)
+	{
+		/* Counts number of characters in text_content */
+		while (text_content[k])
+			k++;
+		/* Writes text_content to file */
+		new_doc = write(fd, text_content, k);
+		if (new_doc == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
