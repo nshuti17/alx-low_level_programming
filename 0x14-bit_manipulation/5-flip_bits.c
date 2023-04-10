4fca1c420b0b2ef19092693804a8e8f581e771a8
nshuti17 <nshutimartial17@gmail.com>
2023-04-10 08:44:06 -0700

    5.main.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * flip_bits - count the number of bit needed
+ * Description: Returns the number of bits you would need to flip
+ * to get from one number to another.
+ * @k: First digit
+ * @j: Second digit tio flip n to
+ * Return: The necessary number of bits to flip to get from n to m
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xor_op = k ^ j;
+	unsigned int counter = 0;
+
+	/* Count number of set bits (bits with 1) in xor_op (xor operator) */
+	while (xor_op)
+	{
+		/* Checks if the last bit is 1 */
+		counter += (xor_op & 1);
+		/* Right shifts the bits by 1 bit */
+		xor_op >>= 1;
+	}
+	return (counter);
+}
