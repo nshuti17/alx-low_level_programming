6a5d122027c6ddc7a1735510debaade556e062dc
nshuti17 <nshutimartial17@gmail.com>
2023-04-10 08:25:24 -0700

    i 1-print_binary.c

--- 0x14-bit_manipulation/1-main.c
+++ 0x14-bit_manipulation/1-main.c
@@ -20,4 +20,3 @@ int main(void)
     printf("\n");
     return (0);
 }
-
--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,19 +1,18 @@
-[200~#include "main.h"
+#include "main.h"
 #include <stdio.h>
 
 /**
- *  * print_binary - converts unsigned int to binary
- *   * @n: Printed in binary format
- *    * Description:  Prints the binary representation of a number
- *     * Return: binary
- *      */
+ * print_binary - converts unsigned int to binary
+ * @n: Printed in binary format
+ * Description:  Prints the binary representation of a number
+ * Return: binary
+ */
 void print_binary(unsigned long int m)
 {
-		if (m > 1){
-					/*right shift operator*/
-					/*shifts all bits towards right by certain numbf 1 bits*/
-					print_binary(m >> 1);
+	if (m > 1)
+		/*right shift operator*/
+		/*shifts all bits towards right by certain numbf 1 bits*/
+		print_binary(m >> 1);
 
-			_putchar((m & 1) + '0');
-}
+	_putchar((m & 1) + '0');
 }
