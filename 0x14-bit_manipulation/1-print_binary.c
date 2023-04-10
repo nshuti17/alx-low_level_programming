1349e6616201ff44fa2860350bc0c47d1dd3fa66
nshuti17 <nshutimartial17@gmail.com>
2023-04-10 07:47:51 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_binary - converts unsigned int to binary
+ * @n: Printed in binary format
+ * Description:  Prints the binary representation of a number
+ * Return: binary
+ */
+void print_binary(unsigned long int y)
+{
+	if (y > 1)
+		/*right shift operator*/
+		/*shifts all bits towards right by certain numbf 1 bits*/
+		print_binary(y >> 1);
+
+	_putchar((y & 1) + '0');
+}
